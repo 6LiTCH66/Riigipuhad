41bec9c5d8121d26b31d1a59330a0db7e64079ca
opilane <opilane@HP-CZC6297B9D>
2020-10-09 10:01:34 +0300

    mai was added

--- Riigipuhad/Riigipuhad/Mai.xaml.cs
+++ Riigipuhad/Riigipuhad/Mai.xaml.cs
@@ -12,9 +12,52 @@ namespace Riigipuhad
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Mai : ContentPage
     {
+        Label lbl, lbl1, lbl2, lbl3;
+        Image img;
         public Mai()
         {
-            InitializeComponent();
+            Title = "Mai";
+            lbl = new Label()
+            {
+                Text = "Kevadpüha",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 24,
+                TextColor = Color.Black,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Padding = new Thickness(10, 10, 10, 10),
+            };
+            lbl1 = new Label()
+            {
+                Text = "01. mai",
+                FontAttributes = FontAttributes.Italic,
+                Padding = new Thickness(10, 10, 10, 10),
+            };
+            lbl2 = new Label()
+            {
+                Text = "Kevadpüha on Eesti riigipüha, mida peetakse 1. mail. Pühaga tähistatakse kevade saabumist.",
+                FontSize = 18,
+                TextColor = Color.Black,
+                Padding = new Thickness(10, 10, 10, 10),
+            };
+            lbl3 = new Label()
+            {
+                Text = "1. mai kui kevadpüha kattub nii rahvusvahelise töörahvapühaga kui ka Eesti rahvakalendri volbripäevaga.",
+                FontSize = 18,
+                TextColor = Color.Black,
+                Padding = new Thickness(10, 10, 10, 10),
+            };
+            img = new Image
+            {
+                Source = ImageSource.FromFile("kevad.jpg"),
+                Margin = new Thickness(15, 20, 15, 5),
+            };
+            StackLayout stackLayout = new StackLayout()
+            {
+                Children = {lbl, lbl1, lbl2, lbl3, img}
+            };
+            ScrollView scrollView = new ScrollView { Content = stackLayout };
+            Content = scrollView;
         }
     }
 }
